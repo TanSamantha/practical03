132b522c5781b4d4e878428f9f7a720c35b6ff5f
155130E <155130E@mymail.nyp.edu.sg>
2016-10-24 16:50:11 +0800

    Initial commit

---
+++ src/demo/BookDBAO.java
@@ -0,0 +1,37 @@
+package demo;
+/**
+ * Created by chitboon on 10/23/15.
+ */
+public class BookDBAO {
+    // get book details given the bookId
+    // this method is dummy for now
+    public BookDetails getBookDetails(String bookId) {
+        // return dummy book for testing now
+        return createDummyBook(bookId);
+    }
+
+    // convenient method to create a dummy book detail
+    // this method is meant for testing
+    private BookDetails createDummyBook(String bookId) {
+        BookDetails details = new BookDetails();
+        details.setBookId(bookId);
+        details.setDescription("Web components for developers talks about how web components can transfor the way you develop application for the web");
+        details.setFirstName("Java");
+        details.setSurname("Expert");
+        details.setInventory(5);
+        details.setOnSale(false);
+        details.setPrice(5.9f);
+        details.setTitle("Web components for developers");
+        details.setYear(2015);
+        return details;
+    }
+
+    public BookDetails getBookDetails2(String bookId){
+        return createDummyBook2(bookId);
+    }
+
+    private BookDetails createDummyBook2(String bookId) {
+        BookDetails details2 = new BookDetails();
+        return details2;
+    }
+}
